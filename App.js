452f89422caa2fc23e0b807f4335f3f318f00cf3
Aaron Paul Jacob <aaron.pauljacob@Aarons-MacBook-Pro.local>
2020-09-15 18:07:09 +0530

    Displaying multiple screens in stack navigator using a single helper page called details and how to customize the details page display by passsing routes and other info

--- App.js
+++ App.js
@@ -2,17 +2,43 @@ import React from 'react';
 import {View,Text, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { SignIn, CreateAccount } from './Screens/Screen';
+import { SignIn, CreateAccount, Home, Profile, Details,Profile2 ,Search, Search2} from './Screens/Screen';
 
 const AuthStack = createStackNavigator();
-// const Tabs=createBottomTabNavigator();
+const Tabs= createBottomTabNavigator();
+
+const HomeTabStack =createStackNavigator();
+const ProfileTabStack = createStackNavigator();
+const SearchStack= createStackNavigator();
+const SearchComponent=()=>(
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search2" component={Search2} />
+  </SearchStack.Navigator>);
+
+
+const HomeTabComponent= () =>{
+  return(  <HomeTabStack.Navigator>
+            <HomeTabStack.Screen name="Home" component={Home} />
+            <HomeTabStack.Screen name="Details" component={Details} />
+          </HomeTabStack.Navigator>
+  )
+}
+
+const ProfileTabComponent=() => (
+  <ProfileTabStack.Navigator>
+    <ProfileTabStack.Screen name={"Profile"} component={Profile} />
+    <ProfileTabStack.Screen name={"Profile2"} component ={Profile2} />
+  </ProfileTabStack.Navigator>
+)
 export default () => (
   <NavigationContainer>
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="SignIn" component={SignIn}  options={{title: "Welcome to SignIn Page"}}/>
-      <AuthStack.Screen name="CreateAccount" component={CreateAccount} options={{title:"Hello wecolme to signup"}}/>
-    </AuthStack.Navigator>
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={HomeTabComponent} />
+      <Tabs.Screen name="Search" component={SearchComponent} />
+    </Tabs.Navigator>
   </NavigationContainer>
 );
 
--- Screens/Screen.js
+++ Screens/Screen.js
@@ -22,15 +22,42 @@ const ScreenContainer = (props) => {
 }
 ;
 
-export const Home = () => (
-    <ScreenContainer >
-        <Text>Master List screen</Text>
-        <View style={styles.button}><Button  title="React Native by Example" onPress={()=>{}} /></View>
-        <View style={styles.button}><Button  title="React Native School" onPress={()=>{}} /></View>
+// export const Home = ({navigation}) => (
+//     <ScreenContainer >
+//         <Text>Welcome to Home screen</Text>
+//         <View style={styles.button}><Button  title="Do something on home" onPress={()=>{}} /></View>
+//         <View style={styles.button}><Button  title="Go to details screen " onPress={()=>{navigation.push("Details")}} /></View>
+//         <View style={styles.button}><Button  title="Drawer" onPress={()=>{}} /></View>
+//     </ScreenContainer>
+// )
+// equivalent class component of above mentioned below
+export class Home extends React.Component{
+    render()
+    {
+      return (  <ScreenContainer >
+        <Text>Welcome to Home screen</Text>
+        <View style={styles.button}>
+            <Button  
+                    title="Item 1 Details" 
+                    onPress={()=>
+                       {this.props.navigation.push("Details", {info:'React Native Details of Item 1', headingLine:"Item 1"} )}} />
+        </View>
+        <View style={styles.button}><Button  title="Item 2 Details " onPress={()=>{this.props.navigation.push("Details", {info:"Details of Item 2 are asdfaosidcoin" , headingLine:"Item2 Details"})}}  /></View>
         <View style={styles.button}><Button  title="Drawer" onPress={()=>{}} /></View>
     </ScreenContainer>
-)
+      )
+    }
+}
 
+export const Details = ({route})=>{
+    console.log(route);
+    return ( <ScreenContainer>
+        <Text> Welcome to Details Page </Text>   
+        {route.params.info && <Text>{route.params.info}</Text>}
+        </ScreenContainer>
+    )
+
+};
 export const SignIn= ({navigation}) => {
     return (
         <ScreenContainer >
@@ -53,10 +80,42 @@ export const CreateAccount= ({navigation}) => {
 
 export const Profile = ({navigation})=>{
     return ( <ScreenContainer>
-        <Text> Welcome to Profile Page </Text>
+        <Text> Welcome to Profile Page </Text>   
+        <View style={styles.button}><Button  title="React Native by Example" onPress={()=>{}} /></View>
+        <View style={styles.button}><Button  title="React Native School" onPress={()=>{}} /></View>
+        <View style={styles.button}><Button  title="Drawer" onPress={()=>{}} /></View>
+    </ScreenContainer>
+    )
+
+};
+
+export const Profile2 = ({navigation})=>{
+    return ( <ScreenContainer>
+        <Text> Welcome to Profile 2 Page </Text>   
+        <View style={styles.button}><Button  title="React Native by Example" onPress={()=>{}} /></View>
+        <View style={styles.button}><Button  title="React Native School" onPress={()=>{}} /></View>
+        <View style={styles.button}><Button  title="Drawer" onPress={()=>{}} /></View>
+    </ScreenContainer>
+    )
+
+};
+export const Search = ({navigation})=>{
+    return ( <ScreenContainer>
+        <Text> Welcome to Search Page </Text>   
+        <View style={styles.button}><Button  title="Search Page" onPress={()=>{}} /></View>
         
     </ScreenContainer>
     )
 
 };
+
+export const Search2 = ({navigation})=>{
+    return ( <ScreenContainer>
+        <Text> Welcome to Search 2 Page </Text>   
+        <View style={styles.button}><Button  title="Press The button to perform detailed search" onPress={()=>{}} /></View>   
+    </ScreenContainer>
+    )
+
+};
+
 
